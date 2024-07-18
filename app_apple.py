93316b05bd90e44e791222ed624a13a3f02efff2
tanyi <2313424489@qq.com>
2024-07-19 00:47:56 +0800

    commit git

--- app_apple.py
+++ app_apple.py
@@ -80,11 +80,11 @@ def create_plot():
 def index():
     return render_template('plot.html')
 
-# if __name__ == "__main__":
-#     app.run(debug=True)
-
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    app.run(debug=True)
+
+# if __name__ == "__main__":
+#     app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
 
 
 
